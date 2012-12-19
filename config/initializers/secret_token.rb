2a4d8ac22a0af299a1ea4b32980b10df59f35abe
Vincent <Vincent@Vincent.local>
2012-12-19 09:24:52 +0800

    added rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GoAroadLabs::Application.config.secret_token = '86ac5c9cb201672f1d0687655cb2a129a823a91f14b6334fe3c0ce9dda728a10c226e5248d6bf13b1cbcde15e82b9c1ab56bc158daf00cb5b9071286cb611d17'
