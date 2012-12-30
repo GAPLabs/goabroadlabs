ce701eff6272c8684bab7ed0ef8e358b71f26f88
Vincent <Vincent@Vincent.local>
2012-12-30 16:53:04 +0800

    for registration. input validation

--- app/controllers/event_controller.rb
+++ app/controllers/event_controller.rb
@@ -3,5 +3,28 @@ class EventController < ApplicationController
     end
 
     def register
+
+        @attendee = WebcampRegistration.new(params[:webcamp_registration])
+        # @attendee.code = 'Code'
+
+        if request.post?
+            @code = params[:code]
+            # @code = Codes.where("code = ?", params[:webcamp_registration][:code]).first
+
+            # @attendee.code_id = @code.id unless @code == nil
+            #logger.debug { @attendee.inspect }
+        end
+        # attendee.first_name = 'Farly'
+        # attendee.last_name = 'Taboada'
+        # attendee.code_id = 1
+        # attendee.date_registered = Date.today
+        # attendee.first_session = 1
+        # attendee.second_session = 2
+        # attendee.email_address = 'farly.taboada@goabroad.com'
+        # attendee.status = 1
+        # attendee.save
+    end
+
+    def create
     end
 end
--- app/models/webcamp_registration.rb
+++ app/models/webcamp_registration.rb
@@ -1,3 +1,4 @@
 class WebcampRegistration < ActiveRecord::Base
-     
+    attr_accessible :code, :first_name, :last_name, :email_address
+    validates :first_name, :presence => "true" 
 end
