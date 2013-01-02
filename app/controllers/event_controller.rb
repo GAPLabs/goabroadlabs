ba3ffb621fdf27c0ee4f6b43e7d9f51e80713224
farly <vftaboada@gmail.com>
2013-01-02 15:50:03 +0800

    cleaned controller. removed comments

--- app/controllers/event_controller.rb
+++ app/controllers/event_controller.rb
@@ -14,35 +14,18 @@ class EventController < ApplicationController
         if request.post?
             @code = params[:code]
             if @attendee.valid?
-                @attendee.date_registered = Date.today
-                @attendee.status = 3
-
                 session[:attendee] = @attendee
 
                 redirect_to webcamp_select_workshop_path 
             end
-            # @code = Codes.where("code = ?", params[:webcamp_registration][:code]).first
-
-            # @attendee.code_id = @code.id unless @code == nil
-            #logger.debug { @attendee.inspect }
-        end
-        # attendee.first_name = 'Farly'
-        # attendee.last_name = 'Taboada'
-        # attendee.code_id = 1
-        # attendee.date_registered = Date.today
-        # attendee.first_session = 1
-        # attendee.second_session = 2
-        # attendee.email_address = 'farly.taboada@goabroad.com'
-        # attendee.status = 1
-        # attendee.save
+        end   
     end
 
     def workshops
         @attendee = session[:attendee]
 
-        @code = Codes.find(@attendee.code_id)
-
         redirect_to webcamp_register_path if @attendee == nil
-        
+
+        @code = Codes.find(@attendee.code_id)        
     end
 end
