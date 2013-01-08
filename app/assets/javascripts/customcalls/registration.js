a5e1edf26754403576345489aa8bb2ef45bb14db
farly <vftaboada@gmail.com>
2013-01-08 16:50:33 +0800

    fixed auto scrolling..

--- app/assets/javascripts/customcalls/registration.js
+++ app/assets/javascripts/customcalls/registration.js
@@ -9,7 +9,7 @@
         
         function scrollToArticle(offset){
             $('html, body').animate({
-                scrollTop: offset.top - 120
+                scrollTop: offset.top - 108
             }, 500);
         }
     });
