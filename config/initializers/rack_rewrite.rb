be6442bab3d6da8f4b0dfa35945455139f9b9f3a
farly <vftaboada@gmail.com>
2013-01-09 10:27:22 +0800

    temporarily changed GoAbroadLabs to GoAroadLabs

--- config/initializers/rack_rewrite.rb
+++ config/initializers/rack_rewrite.rb
@@ -1,4 +1,4 @@
-GoAbroadLabs::Application.config.middleware.insert_before(Rack::Lock, Rack::Rewrite) do
+GoAroadLabs::Application.config.middleware.insert_before(Rack::Lock, Rack::Rewrite) do
     r301 %r{.*}, 'http://www.goabroadlabs.ph$&', :if => Proc.new {|rack_env|
     rack_env['SERVER_NAME'] == 'goabroadlabs.ph'
     }
