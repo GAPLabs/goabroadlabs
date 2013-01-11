7bb7cb5425f3492cf36887ca3818967c94ee0d80
farly <vftaboada@gmail.com>
2013-01-11 11:38:31 +0800

    changed goabroadlabs to gaplabs

--- config/initializers/rack_rewrite.rb
+++ config/initializers/rack_rewrite.rb
@@ -1,5 +1,5 @@
 GoAroadLabs::Application.config.middleware.insert_before(Rack::Lock, Rack::Rewrite) do
-    r301 %r{.*}, 'http://www.goabroadlabs.ph$&', :if => Proc.new {|rack_env|
-    rack_env['SERVER_NAME'] == 'goabroadlabs.ph'
+    r301 %r{.*}, 'http://www.gaplabs.ph$&', :if => Proc.new {|rack_env|
+    rack_env['SERVER_NAME'] == 'gaplabs.ph'
     }
 end
